9e31816e2141253f47ca14ec5fb9193da78c9df8
TURKCELL\EXT02D17424 <konstantin.shevchenko@consultant.turkcell.com.tr>
2019-07-02 21:58:14 +0300

    add gitignore

--- src/main/kotlin/com/koflox/bot/app.kt
+++ src/main/kotlin/com/koflox/bot/app.kt
@@ -4,11 +4,16 @@ import org.telegram.telegrambots.ApiContextInitializer
 import org.telegram.telegrambots.meta.TelegramBotsApi
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException
 
-fun main() {
-    ApiContextInitializer.init()
-    try {
-        TelegramBotsApi().registerBot(GentlemanClubBot())
-    } catch (e: TelegramApiException) {
-        e.printStackTrace()
-    }
+object Main {
+
+        @JvmStatic
+        fun main(args: Array<String>) {
+            ApiContextInitializer.init()
+            try {
+                TelegramBotsApi().registerBot(GentlemanClubBot())
+            } catch (e: TelegramApiException) {
+                e.printStackTrace()
+            }
+        }
+
 }
